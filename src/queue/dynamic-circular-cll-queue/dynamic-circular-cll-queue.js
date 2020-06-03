eeb645a444b7cc974f36646dd4f868ce4a980847
dasathyakuma <dasathyakuma@ebay.com>
2020-06-03 10:04:37 -0600

    Dynamic circular CLL Q

--- src/queue/dynamic-circular-cll-queue/dynamic-circular-cll-queue.js
+++ src/queue/dynamic-circular-cll-queue/dynamic-circular-cll-queue.js
@@ -4,6 +4,17 @@ const {
     CLLNode
 } = require('./node');
 
+
+/**
+ * The dynamic circular Q is implemented here using a Circular Linked List
+ * with a Tail pointer to ref the rear node (acting as a REAR pointer), and the
+ * REAR.next is the FRONT node. As in a normal Linear Q, the circular Q support
+ * insertion at the REAR and deletion at the FRONT. At any point in time, the rear
+ * and front would be different nodes, except when there are no elements (empty Q, where
+ * both point to NULL) and the single element case, where the element would refer to itself
+ * as part of the Node's NEXT pointer reference. This is different from the Linear Q
+ * representation via a SLL (with a rear reference), so that insertion could happen in O(1) time.
+ */
 class DynamicCircularCLLQueue {
     constructor() {
         this.rear = null;
@@ -58,6 +69,9 @@ class DynamicCircularCLLQueue {
             idx = -1; // default case is Node does not exist.
 
         while(currentNode) {
+            // increment at the head
+            idx++;
+
             // if the currentnode's data equals the incoming data, then break.
             // whatever incremented index so far, is the currentIndex at which the Node exists
             if (currentNode.data === data) {
@@ -76,7 +90,6 @@ class DynamicCircularCLLQueue {
 
             // proceed forward with the loop from front to rear.
             currentNode = currentNode.next;
-            idx++;
         }
 
         return idx; // > returns the index at which the node exists
@@ -90,16 +103,21 @@ class DynamicCircularCLLQueue {
 
         let newNode = data;
 
+        // if the incoming data is a Node, just assign. Else wrap a CLLNode over it
         if (!(data instanceof CLLNode)) {
             newNode = new CLLNode(data);
         }
 
-        if (this.isEmpty()) {
+        if (this.isEmpty()) { // circular ref, points to itself
             this.rear = newNode;
             newNode.next = newNode;
         } else {
+            // 1. New Node's next must point to current rear's next
+            // 2. Current rear's NEXT must point to new Node.
+            // 3. Current rear must point to new node
             newNode.next = this.rear.next;
             this.rear.next = newNode;
+            this.rear = newNode;
         }
 
         ++this.length;
@@ -117,10 +135,13 @@ class DynamicCircularCLLQueue {
         let dequeuedValue = this.rear.next;
 
         // then there is only 1 element left in the CLL dynamic Q.
-        // if this is removed, then the pointers have to be reset.
+        // if this is removed, then the pointers have to be reset, back to NULL.
         if (this.rear === this.rear.next) {
             this.rear = null;
         } else {
+            // 1. Current rear's next must point to front's next.
+            // 2. Current fron'ts next must point to NULL (and no longer refer to its next)
+            // 3. Set the current front to NULL.
             let currentFrontNode = this.rear.next;
             this.rear.next = currentFrontNode.next;
             currentFrontNode.next = null;
