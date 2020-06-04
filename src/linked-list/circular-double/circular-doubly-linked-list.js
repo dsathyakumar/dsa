c0a0742e7c48084a79870890fafa16bce06206f8
dasathyakuma <dasathyakuma@ebay.com>
2020-06-04 13:05:34 -0600

    Update circular-doubly-linked-list.js

--- src/linked-list/circular-double/circular-doubly-linked-list.js
+++ src/linked-list/circular-double/circular-doubly-linked-list.js
@@ -5,6 +5,58 @@ class CircularDoublyLinkedList {
         this.length = 0;
         this.tail = null;
     }
+
+    isEmpty() {
+
+    }
+
+    size() {
+
+    }
+
+    lastIndex() {
+
+    }
+
+    display() {
+
+    }
+
+    destroy() {
+
+    }
+
+    get(index) {
+
+    }
+
+    find(value) {
+
+    }
+
+    modify(index, value) {
+
+    }
+
+    updateAll(oldData, newData) {
+
+    }
+
+    reverse() {
+
+    }
+
+    recursiveReverse() {
+
+    }
+
+    insert(data, index) {
+
+    }
+
+    delete(index) {
+
+    }
 }
 
 exports.CircularDoublyLinkedList = CircularDoublyLinkedList;
