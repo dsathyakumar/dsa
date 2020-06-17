9a5ddea094c09b3eeb87275ffd8f12195613578a
dasathyakuma <dasathyakuma@ebay.com>
2020-06-17 16:39:34 -0600

    Update index.js

--- src/binary-tree/index.js
+++ src/binary-tree/index.js
@@ -8,36 +8,45 @@ const {
     BinaryTree
 } = require('./binary-tree');
 
-const bt = new BinaryTree();
+// const bt = new BinaryTree();
 
-// root @ level - 0
-console.log(bt.insert(7));
+// // root @ level - 0
+// console.log(bt.insert(7));
 
-// Level - 1
-console.log(bt.insert(5));
-console.log(bt.insert(10));
+// // Level - 1
+// console.log(bt.insert(5));
+// console.log(bt.insert(10));
 
-// level - 2
-const node3 = new TreeNode(3);
-node3.right = new TreeNode(21);
-console.log(bt.insert(node3));
+// // level - 2
+// const node3 = new TreeNode(3);
+// node3.right = new TreeNode(21);
+// console.log(bt.insert(node3));
 
-console.log(bt.insert(6));
+// console.log(bt.insert(6));
 
-const node9 = new TreeNode(9);
-node9.left = new TreeNode(44);
-console.log(bt.insert(node9));
+// const node9 = new TreeNode(9);
+// node9.left = new TreeNode(44);
+// console.log(bt.insert(node9));
 
 
-const node77 = new TreeNode(77);
-node77.left = new TreeNode(67);
+// const node77 = new TreeNode(77);
+// node77.left = new TreeNode(67);
 
-const node12 = new TreeNode(12);
-node12.right = node77;
+// const node12 = new TreeNode(12);
+// node12.right = node77;
 
-console.log(bt.insert(node12));
+// console.log(bt.insert(node12));
 
-console.log(bt.preOrder());
-console.log(bt.inOrder());
-console.log(bt.postOrder());
-console.log(bt.levelOrder())
+// console.log(bt.preOrder());
+// console.log(bt.inOrder());
+// console.log(bt.postOrder());
+// console.log(bt.levelOrder())
+
+
+// const root = BinaryTree.arrayToBinaryTree([3, null, 4, null, null, null, 5, null, null, null, null, null, null, null, 6]);
+const root = BinaryTree.arrayToBinaryTree([1, 2, 3, 4, 5, 6, 7, null, null, null, null, null, 8, null, 9]);
+console.log(root);
+console.log(BinaryTree.verticalPreOrderTraversal(root));
+console.log('hi');
+
+const morrisPreOrder = BinaryTree.arrayToBinaryTree();
